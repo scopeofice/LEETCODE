5e4a9104e088fd757f45b58739a8055ac9a9f740
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-04-23 03:11:07 +0530

    Create MergArrays.java

---
+++ MergArrays.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int j = 0; j < n; j++) {
+            nums1[m] = nums2[j];
+            m++;
+        }
+        int temp=0;
+        for (int p=0;p<nums1.length;p++){
+            for(int q=p+1;q<nums1.length;q++){
+                if(nums1[q]<nums1[p]){
+                    temp=nums1[q];
+                    nums1[q]=nums1[p];
+                    nums1[p]=temp;
+                }
+            }
+        }
+
+        // Arrays.sort(nums1);
+
+    }
+}
