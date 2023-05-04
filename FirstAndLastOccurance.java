d9030dbc4097d132c60238af32e63dbabef06d8a
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-05-05 00:00:41 +0530

    First And Last Occurance

---
+++ FirstAndLastOccurance.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        if (nums == null || nums.length == 0) {
+            return ans;
+        }
+        int left = Left(nums, target);
+        int right = Right(nums, target);
+        ans[0] = left;
+        ans[1] = right;
+        return ans;
+    }
+    
+    private int Left(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (left < nums.length && nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+    
+    private int Right(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (right >= 0 && nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+}
