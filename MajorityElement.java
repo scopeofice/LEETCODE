f70c1298d35f88d52d6631caff6d77c1d828dc2a
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-06-26 06:47:44 +0530

    Majority Element

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,6 +1,5 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
         for(int i=0;i<nums.length;i++){
             int count=0;
             for(int j=0;j<nums.length;j++){
