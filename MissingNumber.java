19241876246eae57f271eeca6cecc4a6ff778f34
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-07-04 09:27:52 +0530

    MissingNumber

--- MissingNumber.java
+++ MissingNumber.java
@@ -10,3 +10,13 @@ class Solution {
     
     }
 }
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int ans=0;
+//         for(int i=0;i<nums.length;i++){
+//             ans+=((i+1)-nums[i]);
+//         }   
+//         return ans;
+//     }
+// }
+
