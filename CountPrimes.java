2eebc4b52638891d625cbea402b9da721a57a98a
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-07-07 21:18:48 +0530

    Count Primes

---
+++ CountPrimes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean arr[]=new boolean[n];
+        long c=0;
+        for(long i=2;i<n;i++){
+            if(arr[(int)i])
+            continue;
+            c++;
+            for(long j=i*i;j<n;j+=i){
+                arr[(int)j]=true;
+            }
+        }
+        return (int)c;
+    }
+}
