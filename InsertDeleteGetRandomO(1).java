72ac00cc0510b6d8e5ce194ffa990088e65933d5
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-08-15 12:11:10 +0530

    Insert Delete Get Random O(1)

---
+++ InsertDeleteGetRandomO(1).java
@@ -0,0 +1,38 @@
+class RandomizedSet {
+    Set<Integer> set;
+    public RandomizedSet() {
+        set= new HashSet<>();
+    }
+    
+    public boolean insert(int val) {
+        if(set.contains(val)){
+            return false;
+        }else{
+            set.add(val);
+            return true;
+        }
+    }
+    
+    public boolean remove(int val) {
+        if(set.contains(val)){
+            set.remove(val);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public int getRandom() {
+        List<Integer> list=new ArrayList<>(set);
+        int r=(int)(Math.random()*set.size());
+        return list.get(r);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
